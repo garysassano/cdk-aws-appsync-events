e18f677fc76505539a4a98bbd6360754a63b052c
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-05-04 16:51:48 +0200

    initial commit

---
+++ .projenrc.ts
@@ -0,0 +1,54 @@
+import { awscdk, javascript } from "projen";
+import { IndentStyle } from "projen/lib/javascript/biome/biome-config";
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  name: "cdk-aws-appsync-events",
+
+  // Base
+  defaultReleaseBranch: "main",
+  depsUpgradeOptions: { workflow: false },
+  projenrcTs: true,
+
+  // Toolchain
+  biome: true,
+  biomeOptions: {
+    biomeConfig: {
+      assist: {
+        enabled: true,
+        actions: {
+          source: {
+            organizeImports: "on",
+          },
+        },
+      },
+      formatter: {
+        enabled: true,
+        indentStyle: IndentStyle.SPACE,
+        indentWidth: 2,
+        lineWidth: 100,
+      },
+      linter: {
+        enabled: true,
+        rules: {
+          recommended: true,
+          suspicious: {
+            noShadowRestrictedNames: "off",
+          },
+        },
+      },
+    },
+  },
+  cdkVersion: "2.206.0",
+  minNodeVersion: "22.17.1",
+  packageManager: javascript.NodePackageManager.PNPM,
+  pnpmVersion: "10",
+
+  // Deps
+  deps: [
+    "@aws-lambda-powertools/event-handler",
+    "@aws-lambda-powertools/logger",
+    "@types/aws-lambda",
+  ],
+});
+
+project.synth();
